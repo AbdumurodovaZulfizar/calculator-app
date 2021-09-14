f3023f15455f18af3851b79554bcccaaaa221b8e
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-14 12:07:32 +0500

    Fix eslint erors

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Heloooooo World
+          Edit
+          <code>src/App.js</code>
+          and save to reload. Heloooooo World
         </p>
         <a
           className="App-link"
