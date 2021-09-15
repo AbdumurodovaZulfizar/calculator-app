b1ac1e531ef68148f1f98d4c256aa64f614fde9e
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-15 11:38:16 +0500

    Remove unnecery assets and files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          <code>src/App.js</code>
+          <code>src/App.js </code>
           and save to reload. Heloooooo World
         </p>
         <a
