3da17fde26a7ad46b814ee2b77ba12408fa00466
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-16 22:00:57 +0500

    Remove stylelint-disable

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,50 +1,49 @@
 import React from 'react';
 import Button from './buttons/button';
 
-/* eslint-disable */
+// /* eslint-disable */
 
 const ordinaries = ['AC', '+/-', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3'];
-const lasts = ['.', '=']
-const oranges =['/', 'x', '-', '+']
+const lasts = ['.', '='];
+const oranges = ['/', 'x', '-', '+'];
 
-const ButtonPanel = () => {
+const ButtonPanel = () => (
+  <div className="buttonPanel row p-0 m-0">
+    <div className="ordinaries col-9 p-0">
+      {ordinaries.map((numbers) => (
+        <Button
+          className="ordinary"
+          buttonNames={numbers}
+          classNameTwo="color"
+          key={numbers}
+        />
+      ))}
+      <Button
+        className="width-half"
+        buttonNames="0"
+        classNameTwo="color"
+        key="0"
+      />
+      {lasts.map((last) => (
+        <Button
+          className="ordinary"
+          buttonNames={last}
+          classNameTwo="color"
+          key={last}
+        />
+      ))}
+    </div>
+    <div className="oranges col-3 p-0">
+      {oranges.map((orange) => (
+        <Button
+          className="orange"
+          buttonNames={orange}
+          classNameTwo=""
+          key={orange}
+        />
+      ))}
+    </div>
+  </div>
+);
 
-    return (
-      <div className="buttonPanel row p-0 m-0">
-        <div className='ordinaries col-9 p-0'>
-          {ordinaries.map((numbers) => (
-            <Button
-            className={"ordinary"}
-            buttonNames={numbers}
-            classNameTwo={"color"}
-            />
-          ))}
-          <Button
-          className={"width-half"}
-          buttonNames={"0"}
-          classNameTwo={"color"}
-          />
-          {lasts.map((last) => (
-            <Button
-            className={"ordinary"}
-            buttonNames={last}
-            classNameTwo={"color"}
-            />
-          ))}
-        </div>
-        <div className='oranges col-3 p-0'>
-            {
-              oranges.map((orange) => (
-                <Button
-                className={"orange"}
-                buttonNames={orange}
-                classNameTwo={""}
-                />
-              ))
-            }
-        </div>
-      </div>
-    )
-  }
-
-  export default ButtonPanel;
+export default ButtonPanel;
