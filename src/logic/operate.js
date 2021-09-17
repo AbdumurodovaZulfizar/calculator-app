714cf8a6ceb41274a89fcab1abe5bd79e18e6820
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-17 09:13:34 +0500

    Create operate function

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,23 @@
-import Big from "big.js";
+import Big from 'big.js';
+
+const operate = (number1, number2, operation) => {
+  const numberOne = Number(number1);
+  const numberTwo = Number(number2);
+
+  switch (operation) {
+    case '+':
+      return Big(numberOne) + Big(numberTwo);
+    case '-':
+      return Big(numberOne) - Big(numberTwo);
+    case '/':
+      return Big(numberOne) / Big(numberTwo);
+    case '*':
+      return Big(numberOne) * Big(numberTwo);
+    case '%':
+      return Big(numberOne) % Big(numberTwo);
+    default:
+      return null;
+  }
+};
+
+export default operate;
