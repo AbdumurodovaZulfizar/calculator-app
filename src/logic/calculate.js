5ac8c97d037c0e23f163926638d289cf4827b7e3
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-17 10:14:25 +0500

    Add calculate function to the logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,1 +1,37 @@
-import Big from "big.js";
+import operate from './operate';
+
+const calculate = (object, buttonNames) => {
+  const NewObj = { ...object };
+
+  let { total, next, operation } = NewObj;
+  if (buttonNames === 'AC') {
+    total = '';
+    next = '';
+    operation = null;
+  } else if (
+    buttonNames === '+'
+    || buttonNames === '-'
+    || buttonNames === '*'
+    || buttonNames === '/'
+    || buttonNames === '%'
+  ) {
+    operation = !next ? buttonNames : null;
+  } else if (buttonNames === '+/-') {
+    if (next) {
+      next = (next * -1).toString();
+    } else if (total) {
+      total = (total * -1).toString();
+    }
+  } else if (buttonNames === '=') {
+    total = operate(total, next, operation);
+    next = '';
+    operation = null;
+  } else if (!operation) {
+    total += buttonNames;
+  } else {
+    next += buttonNames;
+  }
+  return { total, next, operation };
+};
+
+export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,20 +1,22 @@
 import Big from 'big.js';
 
 const operate = (number1, number2, operation) => {
+  if (!number1 || !number2 || !operation) return '0';
+
   const numberOne = Number(number1);
   const numberTwo = Number(number2);
 
   switch (operation) {
     case '+':
-      return Big(numberOne) + Big(numberTwo);
+      return (Big(numberOne) + Big(numberTwo)).toString();
     case '-':
-      return Big(numberOne) - Big(numberTwo);
+      return (Big(numberOne) - Big(numberTwo)).toString();
     case '/':
-      return Big(numberOne) / Big(numberTwo);
+      return (Big(numberOne) / Big(numberTwo)).toString();
     case '*':
-      return Big(numberOne) * Big(numberTwo);
+      return (Big(numberOne) * Big(numberTwo)).toString();
     case '%':
-      return Big(numberOne) % Big(numberTwo);
+      return ((Big(numberOne) * Big(numberTwo)) / 100).toString();
     default:
       return null;
   }
