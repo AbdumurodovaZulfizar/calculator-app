6a5617287221a877fe180306ad88c1cdb5e94c77
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-23 18:01:45 +0500

    Create Home page

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../index.css';
+
+const Home = () => (
+  <div className='home-page'>
+    <h2>This is Math Magician Website`&apos`s Home Page</h2>
+    <div>
+      <p>
+        Nothing to say!
+      </p>
+    </div>
+  </div>
+);
+
+export default Home;
