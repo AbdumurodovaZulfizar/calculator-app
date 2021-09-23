d322eb41b820a473fb1572a141a438f064ed367e
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-23 21:34:29 +0500

    Style all pages end

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,8 +3,15 @@ import '../index.css';
 
 const Quote = () => (
   <div className="text-dark">
-    <h2 className="text-dark">Quote Page</h2>
-    <p className="text-dark">Bla Bla Bla</p>
+    <h2 className="text-dark fw-light text-center">Quote Page</h2>
+    <p className="text-dark fs-5 text-center">
+      Mathematics are the result of mysterious powers
+      which no one understands, and which the unconscious
+      recognition of beauty must play an important part.
+      Out of an infinity of designs a mathematician chooses
+      one pattern for beauty’s sake and pulls it down to earth.
+    </p>
+    <p className="text-dark float-end fw-bold">— Sofia Kovalevskaya, Russian mathematician</p>
   </div>
 );
 
