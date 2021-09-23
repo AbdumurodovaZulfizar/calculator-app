3e5349db13e905c8f1cdb337803d06a3ae244a2b
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-23 17:52:25 +0500

    Create calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../index.css';
+import App from './App';
+
+const Calculator = () => (
+  <div>
+    <h2>Calculator App</h2>
+    <div>
+      <App />
+    </div>
+  </div>
+);
+
+export default Calculator;
