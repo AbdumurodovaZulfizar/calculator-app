ff91c9235c816f7af5267d74d8eba86768bafa87
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-23 18:29:31 +0500

    Replace according to modification

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import AppRouter from './components/Router';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById('App'),
 );
