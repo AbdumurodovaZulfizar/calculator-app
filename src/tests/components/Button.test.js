e4f4bbcf85935e34922745c068b807da8ff0364c
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-24 10:50:46 +0500

    Create tests for button Component

---
+++ src/tests/components/Button.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Button from '../../components/buttons/button';
+
+test('creates button element', () => {
+  render(<Button buttonNames="5" />);
+  const newButton = screen.getByTestId('button');
+  expect(newButton).toHaveTextContent('5');
+});
+
+test('creates operation buttons', () => {
+  render(<Button buttonNames="/" />);
+  const operateButton = screen.getByRole('button');
+  expect(operateButton).toHaveTextContent('/');
+});
