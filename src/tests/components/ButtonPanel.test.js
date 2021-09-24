2d4e6f98d2be34505f77c56912fe1d4674fcebb7
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-24 11:01:23 +0500

    Fix errors

--- src/tests/components/ButtonPanel.test.js
+++ src/tests/components/ButtonPanel.test.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import ButtonPanel from "../../components/ButtonPanel";
+import ButtonPanel from '../../components/ButtonPanel';
 
 test('creates buttonPanel', () => {
   render(<ButtonPanel />);
